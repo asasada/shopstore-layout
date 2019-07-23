19a2be55e92e16a686cba14aa59033713cc01156
asasada <mpa0021@gmail.com>
2019-07-23 22:19:20 +0300

    Add files via upload
    wrapping of gallery added

--- basket-appearance.js
+++ basket-appearance.js
@@ -1,9 +1,11 @@
 let basket = document.getElementById('basket');
 
 document.getElementById('basket-link').onclick = function () {
-    basket.style.display = "block";
+    //basket.style.display = "block";
+    basket.style.right = "0";
 };
 
 document.getElementById('close-basket').onclick = function () {
-    basket.style.display = "none";
+    //basket.style.display = "none";
+    basket.style.right = "-340px";
 };
--- gallery.js
+++ gallery.js
@@ -4,9 +4,20 @@ let galleries = document.querySelectorAll('.gallery');
 galleries.forEach(function(gallery) {
     let littleImages = gallery.children;
     for (let i = 1; i < littleImages.length; i++) {
-        //finding a main image
-        littleImages[i].addEventListener( "click" , function() {
-            this.parentElement.previousElementSibling.src = this.src;
-        });
+        if (littleImages[i].tagName === "IMG") {
+            littleImages[i].addEventListener("click", function () {
+                this.parentElement.previousElementSibling.src = this.src;
+            });
+        }
     }
 });
+
+function leftScroll(gallery) {
+
+    gallery.style.justifyContent = "flex-start";
+}
+
+function rightScroll(gallery) {
+    gallery.style.justifyContent = "flex-end";
+}
+
